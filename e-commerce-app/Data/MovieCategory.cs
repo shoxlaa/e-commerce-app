366399d9e86809f2ec19354c0691ee05fa5ca609
shahla.g <shahla.guliyeva@cybernet.az>
2023-10-07 13:20:07 +0400

    Added models and DbContext

---
+++ e-commerce-app/Data/MovieCategory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace e_commerce_app.Data
+{
+    public enum MovieCategory
+    {
+        Action=1  , 
+        Comedy , 
+        Drama , 
+        Documentary 
+    }
+
+    public class AppDbContext :DbContext
+    {
+
+    }
+}
--- e-commerce-app/Models/Actor.cs
+++ e-commerce-app/Models/Actor.cs
@@ -1,7 +1,46 @@
-﻿namespace e_commerce_app.Models
+﻿using e_commerce_app.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace e_commerce_app.Models
 {
     public class Actor
     {
+        [Key]
+        public int ActorId { get; set; }  
+        public string ProfilePictureUrl { get; set; }
+        public string Bio { get; set; } 
+
+    }
+
+    public class Producer
+    {
+        [Key]
+        public int ProducerId { get; set;}
+        public string ProfilePictureUrl { get; set; }
+
+        public string FullName { get; set; }
+        public string Bio { get; set;} 
+    }
+
+    public class Cinema
+    {
+        [Key]   
+        public int CinemaId { get; set; }
+        public string Logo { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Movie
+    {
+        [Key]
+        public int MovieId { get; set; }     
+        public string Name { get; set; }
+        public string Description { get; set; }  
+        public double Price { get ; set; }   
+        public DateTime StartDate { get; set; } 
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
     }
 }
 
