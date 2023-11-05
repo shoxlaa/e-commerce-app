d22d606052e8617e432ba16003fc9d1d7be58fc5
shahla.g <shahla.guliyeva@cybernet.az>
2023-11-05 22:28:43 +0400

    cat

--- e-commerce-app/Data/MovieCategory.cs
+++ e-commerce-app/Data/MovieCategory.cs
@@ -12,6 +12,9 @@ namespace e_commerce_app.Data
 
     public class AppDbContext :DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
 
+        }
     }
 }
