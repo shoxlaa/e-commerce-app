8ac955845e6e5ef31c9a0083815f49110b677665
shahla.g <shahla.guliyeva@cybernet.az>
2023-12-25 10:43:31 +0400

    Add Migration rename id column

--- e-commerce-app/Migrations/20231225062837_Changed the Id Titles in models.Designer.cs
+++ e-commerce-app/Migrations/20231225062837_Changed the Id Titles in models.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using e_commerce_app.Data;
 
@@ -11,9 +12,11 @@ using e_commerce_app.Data;
 namespace e_commerce_app.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231225062837_Changed the Id Titles in models")]
+    partial class ChangedtheIdTitlesinmodels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ e-commerce-app/Migrations/20231225062837_Changed the Id Titles in models.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace e_commerce_app.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedtheIdTitlesinmodels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProducerId",
+                table: "Producers",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "MovieId",
+                table: "Movies",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "CinemaId",
+                table: "Cinemas",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "ActorId",
+                table: "Actors",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Actors",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Producers",
+                newName: "ProducerId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Movies",
+                newName: "MovieId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Cinemas",
+                newName: "CinemaId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Actors",
+                newName: "ActorId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Actors",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
